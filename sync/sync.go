f0a43805211bad0ec038d891f4f5a12c04f4d596
lyckade <as@lycka.de>
2016-06-27 20:28:58 +0200

    Sync function is ready

--- sync/sync.go
+++ sync/sync.go
@@ -7,19 +7,39 @@ import "errors"
 //time. If both have the same time that error have to be returned.
 var ErrFilesHaveSameAge = errors.New("Both files have the same last change time.")
 
-//The Syncer interface abstracts the sync process for a simple
-//testing of the sync logik.
+//The Filesyncer interface abstracts the sync process for syncing a
+//file.
 //A file is represented by a string. The string should be a the path
 //to the file.
-type Syncer interface {
+type Filesyncer interface {
 	//Copy is used to copy the source to the destination
 	//Error is thrown, when it is not possible to copy
 	//the file.
 	Copy(string, string) error
-	//GetOlderFile returns the filpath ot the older file as string
-	GetOlderFile(string, string) (string, error)
 	//GetNewerFile returns the filepath of the newer file as string
 	GetNewerFile(string, string) (string, error)
 	//Returns true if a file should be not synced
 	SkipFile(string) bool
+}
+
+//Sync syncs a file. The newer file is copied.
+//A implementation of the filesyncer interface is the basis for
+//all the file functions.
+func Sync(fs Filesyncer, filePath1, filePath2 string) error {
+	if fs.SkipFile(filePath1) {
+		return nil
+	}
+	var dst string
+	src, err := fs.GetNewerFile(filePath1, filePath2)
+	if err != nil {
+		return err
+	}
+	if filePath2 == src {
+		dst = filePath1
+	} else {
+		dst = filePath2
+	}
+	err = fs.Copy(src, dst)
+
+	return err
 }
--- sync/sync_test.go
+++ sync/sync_test.go
@@ -2,6 +2,61 @@ package sync
 
 import "testing"
 
-func TestSync(t *testing.T) {
+type fsTester struct {
+	copysrc string
+	copydst string
+}
 
+func (fs *fsTester) Copy(src, dst string) error {
+	fs.copysrc = src
+	fs.copydst = dst
+	return nil
+}
+func (fs *fsTester) GetNewerFile(f1, f2 string) (string, error) {
+	switch {
+	case f1 == "Test2" && f2 == "Test3":
+		return "", ErrFilesHaveSameAge
+	case f2 == "Test1":
+		return f2, nil
+	default:
+		return f1, nil
+	}
+}
+
+func (fs *fsTester) SkipFile(s string) bool {
+	if s == "TestSkip" {
+		return true
+	}
+	return false
+}
+
+var tests = []struct {
+	file1     string
+	file2     string
+	expectsrc string
+	expectdst string
+	expecterr error
+}{
+	{"Test1", "Test2", "Test1", "Test2", nil},
+	{"Test2", "Test1", "Test1", "Test2", nil},
+	{"Test2", "Test3", "", "", ErrFilesHaveSameAge},
+	{"TestSkip", "Test4", "", "", nil},
+}
+
+func TestSync(t *testing.T) {
+	for _, test := range tests {
+		fs := &fsTester{}
+		err := Sync(fs, test.file1, test.file2)
+		if err != test.expecterr {
+			t.Fatalf("Wrong Error. Got:%v Expect: %v\n", err, test.expecterr)
+		}
+		if fs.copysrc != test.expectsrc && fs.copydst != test.expectdst {
+			t.Fatalf("Copy is not called correct!\n Got: src:%v dst:%v\nWant: src:%v dst:%v",
+				fs.copysrc,
+				fs.copydst,
+				test.expectsrc,
+				test.expectdst)
+		}
+
+	}
 }
