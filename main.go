e4e6e1a8cb6b862540524e615548c5fd40fd4f4e
lyckade <as@lycka.de>
2016-06-29 22:12:48 +0200

    Implementing the logik

--- main.go
+++ main.go
@@ -1,17 +1,28 @@
 package main
 
 import (
+	"errors"
 	"flag"
 	"fmt"
+	"log"
 	"os"
+	"path/filepath"
+
+	"github.com/lyckade/gosync/osfsyncer"
+	"github.com/lyckade/gosync/sync"
 )
 
 // Version defines the version number of the app
 const Version = "0.7.0"
 
+var ErrNotEnoughArguments = errors.New("Not enough arguments!")
+
 var flagVersion = flag.Bool("version", false, "Version of this app.")
 var flagVerbose = flag.Bool("v", false, "increase verbosity")
-var flagLog = flag.Bool("log", false, "Actions are written to gosync.log")
+
+//var flagLog = flag.Bool("log", false, "Actions are written to gosync.log")
+
+var logger = log.New(os.Stdout, "", log.LstdFlags)
 
 func init() {
 	flag.Parse()
@@ -19,7 +30,41 @@ func init() {
 
 func main() {
 	if *flagVersion {
-		fmt.Printf("Version: %s", Version)
+		fmt.Printf("Version: %s\n", Version)
 		os.Exit(-1)
 	}
+	if len(os.Args) < 3 {
+		fmt.Print(ErrNotEnoughArguments)
+	}
+	syncFolder := os.Args[1]
+	distFolder := os.Args[2]
+	logger.Printf("Sync started for %s\n", syncFolder)
+	filepath.Walk(syncFolder, func(fpath string, info os.FileInfo, err error) error {
+		if info.IsDir() {
+			return err
+		}
+		/*ignore, err := MatchPath(fpath, properties.Ignore)
+		if err != nil || ignore {
+			fmt.Println(fpath)
+			return err
+		}*/
+		var syncer osfsyncer.Osfsyncer
+		dpath, err := sync.MakeDistPath(fpath, syncFolder, distFolder)
+		if err != nil {
+			logger.Fatalln(err)
+		}
+		lg(fpath)
+		err = sync.Sync(&syncer, fpath, dpath)
+		if err != nil {
+			logger.Fatalln(err)
+		}
+		return err
+	})
+}
+
+func lg(s string) {
+	if *flagVerbose {
+		logger.Println(s)
+	}
+	logger.Println(s)
 }
